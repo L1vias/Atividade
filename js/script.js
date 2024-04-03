e8048a72d1b88e0e522975e21745ec026e7a2988
Solla <liviasolla552@gmail.com>
2024-04-03 17:03:25 -0300

    Atividade1-25

--- js/script.js
+++ js/script.js
@@ -88,3 +88,192 @@ const exercicio9 = () => {
 
   result.innerText = `${somar}`;
 };
+
+//-----------------------------------
+const exercicio8 = () => {
+  let palavra = document.getElementById("palavra").value;
+  let error = document.getElementById("erro");
+  let result = document.getElementById("resultado");
+  error.innerText = "";
+  result.innerText = "";
+
+  let inversa = "";
+
+  if (palavra == "") {
+    error.innerText = "Favor informar uma palavra!";
+  } else {
+    for (let i = 1; i <= palavra.length; i++) {
+      inversa += palavra.charAt(palavra.length - i);
+    }
+
+    palavra.toLowerCase() == inversa.toLowerCase()
+      ? (result.innerText = "A palavra é um palíndromo!")
+      : (error.innerText = "A palavra não é um palíndromo!");
+  }
+};
+
+//----------------------------------------------------
+const exercicio10 = () => {
+  let num = document.getElementById("num").value;
+  let resposta = document.getElementById("resultado");
+  let error = document.getElementById("erro");
+  let contador = 0;
+  let somar = 0;
+  resposta.innerHTML = "";
+  error.innerText = "";
+
+  if (num <= 0) {
+    error.innerText = "Favor informar um número posítivo válido";
+  } else {
+    while (contador <= num) {
+      somar += contador;
+      contador++;
+    }
+
+    let media = somar / num;
+    resposta.innerHTML += `a soma é ${somar}<br>`;
+    resposta.innerHTML += `A média é ${media}`;
+  }
+};
+//----------------------------------------------------------------------
+const exercicio11 = () => {
+  let result = document.getElementById("resposta");
+  for (let i = 0; i < 100; i++) {
+    if (i % 3 == 0) result.innerHTML += `#${i}<br>`;
+  }
+};
+
+//---------------------------------------------------------------------
+const exercicio12 = () =>{
+  let num = document.getElementById("num").value;
+  let result = document.getElementById("resultado");
+  let soma = 0;
+  result.innerHTML = "";
+
+if (num< 10) {
+  error.innerHTML = "Favor informar um número de DOIS digitos ou mais!";
+}else{
+  for (let i = 0; i < num.length; i++) {
+    soma += parseInt(num.charAt(i));
+    
+  }
+  }
+  result.innerText =`A soma dos digitos ${num} é ${soma}`;
+};
+//-------------------------------------------------------------
+const exercicio13 = () =>{
+  let num1 = document.getElementById("num1").value;
+  let num2 = document.getElementById("num2").value;
+  let result = document.getElementById("resultado");
+  let error = document.getElementById("erro");
+let maior, menor;
+
+if (num1 < num2){
+  menor = num1;
+  maior = num2;
+}else {
+  menor = num2;
+  maior = num1;
+}
+
+result.innerText = "";
+if (num1 <= 0 || num2 <= 0){
+  error.innerText =`O número ${num1} ou o número ${num2} não é valido`;
+  result.innerText = "";
+}else{
+  error.innerText = "";
+ result.innerText = "";
+
+ for (let i = menor; i <= maior; i++) {
+  let teste = verificaPrimo(i);
+  if(teste){
+    result.innerText += `${i}`
+  }
+ }
+ }
+};
+
+function verificaPrimo(numero){
+  let primo = true; 
+  for (let i = 2; i < numero; i++) {
+    if(numero % i == 0) primo = false;
+  }
+  return primo
+}
+//----------------------------------------------------
+const exercicio14 = () => {
+  let num1 = document.getElementById("num1").value
+  let num2 = document.getElementById("num2").value
+  let error = document.getElementById("erro")
+  let result = document.getElementById("resultado")
+  let area= num1 * num2
+
+  if (num1 <= 0 || num2 <= 0){
+    error.innerHTML = "Favor informar um número positivo";
+    result.innerHTML = "";
+  }else{
+    error.innerText = "";
+    result.innerText = `A área do retângulo é ${area}`;
+  }
+}
+
+// const exercicio15 = () => {
+//   let palavra = document.getElementById("palavra").value;
+//   let vogais =  [],
+//   consoante = [];
+//   let error = document.getElementById("erro");
+//   let result = document.getElementById("resultado")
+
+//   if()
+// }
+//---------------------------------------------------------------
+const exercicio16 = () => {
+  let num1 = document.getElementById("num1").value
+  let result = document.getElementById("resultado")
+  let error = document.getElementById("erro")
+  let pi = 3.14;
+  let area= pi * num1 ** 2;
+
+  if (num1 <= 0) {
+    result.innerHTML = "";
+    error.innerText = "Favor informar um número positivo";
+  } else{
+    error.innerText = "";
+    result.innerText = `A área do circulo é ${area}`;
+  }
+};
+//------------------------------------------------------------------
+
+const exercicio17 = () => {
+  let num1 = document.getElementById("num1").value
+  let num2 = document.getElementById("num2").value
+  let error = document.getElementById("erro");
+  let result = document.getElementById("resultado");
+  let area= (num1 * num2) / 2;
+
+  if (num1 <= 0 || num2 <= 0 ){
+    error.innerHTML = `Favor informar um número positivo`;
+    result.innerHTML = "";
+  }else{
+    error.innerText = "";
+    result.innerText = `A área do retângulo é ${area}`;
+  }
+}
+
+const exercicio18 = () => {
+  let num1 = document.getElementById("num1").value * 1;
+  let num2 = document.getElementById("num2").value * 1;
+  let num3 = document.getElementById("num3").value * 1;
+  let error = document.getElementById("erro");
+  let result = document.getElementById("resultado");
+  let area= (num1 + num2)* num3 / 2;
+
+  if (num1 <= 0 || num2 <= 0 || num3 <= 0) {
+    error.innerHTML = `Favor informar um número positivo`;
+    result.innerHTML = "";
+  }else{
+    error.innerText = "";
+    result.innerText = `A área do trapézio é ${area}`;
+  }
+}
+
